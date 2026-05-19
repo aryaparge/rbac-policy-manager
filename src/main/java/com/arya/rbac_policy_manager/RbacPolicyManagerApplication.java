1040dcabfe4e0555c49728b42c6f88b1c9e096a5
arya <aryaparge@icloud.com>
2026-05-19 20:38:47 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/arya/rbac_policy_manager/RbacPolicyManagerApplication.java
@@ -0,0 +1,13 @@
+package com.arya.rbac_policy_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RbacPolicyManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RbacPolicyManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arya/rbac_policy_manager/RbacPolicyManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arya.rbac_policy_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RbacPolicyManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
