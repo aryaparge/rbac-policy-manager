2168c85991ba76da4b2f10ac422279be7ac9b651
arya <aryaparge@icloud.com>
2026-05-25 00:35:08 +0530

    basic environment config

--- src/test/java/com/arya/rbac_policy_manager/RbacPolicyManagerApplicationTests.java
+++ src/test/java/com/arya/rbac_policy_manager/RbacPolicyManagerApplicationTests.java
@@ -1,12 +1,9 @@
 package com.arya.rbac_policy_manager;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class RbacPolicyManagerApplicationTests {
 
-	@Test
 	void contextLoads() {
 	}
 
